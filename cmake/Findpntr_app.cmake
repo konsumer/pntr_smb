4893ce364f5f84d0ab54cd001c7367e8f843b301
David Konsumer <konsumer@jetboystudio.com>
2024-12-15 19:38:32 -0800

    older pntr_app makes web sound work

--- cmake/Findpntr_app.cmake
+++ cmake/Findpntr_app.cmake
@@ -1,5 +1,14 @@
 include(FetchContent)
-FetchContent_Declare(pntr_app
-    URL "https://github.com/robloach/pntr_app/archive/refs/heads/master.zip"
+
+# FetchContent_Declare(pntr_app
+#     URL "https://github.com/robloach/pntr_app/archive/refs/heads/master.zip"
+# )
+
+
+FetchContent_Declare(
+    pntr_app
+    GIT_REPOSITORY https://github.com/robloach/pntr_app.git
+    GIT_TAG 2b1c6fd8d5
 )
+
 FetchContent_MakeAvailable(pntr_app)
